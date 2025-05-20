00ca0853d27c6a016459c330ce6f66aa4df953b7
Alexander Kolesov <vvruspat@gmail.com>
2025-05-20 23:14:49 +0200

    remove react native

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -1,40 +1,9 @@
 "use client";
 
 import React, { type ComponentProps } from "react";
-import {
-	Pressable,
-	Text,
-	StyleSheet,
-	type ViewStyle,
-	type TextStyle,
-} from "react-native";
 
-type Props = ComponentProps<typeof Pressable> & {
-	title: string;
-	onPress?: () => void;
-	style?: ViewStyle;
-	textStyle?: TextStyle;
-	children: string;
+type Props = ComponentProps<"button">;
+
+export const Button = ({ children, ...restButtonProps }: Props) => {
+	return <button {...restButtonProps}>{children}</button>;
 };
-
-export const Button = ({ children, onPress, style, textStyle }: Props) => {
-	return (
-		<Pressable onPress={onPress} style={[styles.button, style]}>
-			<Text style={[styles.text, textStyle]}>{children}</Text>
-		</Pressable>
-	);
-};
-
-const styles = StyleSheet.create({
-	button: {
-		backgroundColor: "#007aff",
-		paddingVertical: 10,
-		paddingHorizontal: 16,
-		borderRadius: 8,
-	},
-	text: {
-		color: "#fff",
-		fontWeight: "600",
-		textAlign: "center",
-	},
-});
