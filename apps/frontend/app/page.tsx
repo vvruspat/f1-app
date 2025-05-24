067b1c25d8aa2215ba9500ac4e81622f8a4207c6
Aleksandr Kolesov <aleksandrkolesov@Aleksandrs-MacBook-Pro.local>
2025-05-24 13:37:29 +0200

    docker local

--- apps/frontend/app/page.tsx
+++ apps/frontend/app/page.tsx
@@ -33,7 +33,7 @@ export default function Home() {
 				/>
 				<ol>
 					<li>
-						Get started by editing <code>apps/frontend/app/page.tsx</code>
+						hgjff jgfj gh fjhGet started by editing <code>apps/frontend/app/page.tsx</code>
 					</li>
 					<li>Save and see your changes instantly.</li>
 				</ol>
